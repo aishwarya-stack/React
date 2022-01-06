59d01d8dd22ee50dd15e074e80460971374f084b
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2022-01-06 11:47:36 +0530

    Added:jsx added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import React,{ Component } from 'react';
 import Greet from './component/Greet';
 import Welcome from './component/Welcome';
+import Student from './component/Student';
+import Hello from './component/Hello';
 class App extends Component{
   render(){
     return(
       <div className='App'>
        <Greet/>
-       
+       <Student/>
        <Welcome/>
+       <Hello/>
       </div>
     );
   }
---
+++ src/component/Student.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Student = () =>
+
+    <h1>Hello student</h1>
+
+
+export default Student
